0c4c1ac3eb3bfe874e5d8f2172c1a4e1fb8f9f76
Jiawen Chen <sophiachen1130@gmail.com>
2018-11-12 00:16:16 -0600

    Add Map component

--- src/Component/MapContainer.js
+++ src/Component/MapContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Map, GoogleApiWrapper} from 'google-maps-react'
+import {Map, GoogleApiWrapper, InfoWindow, Marker} from 'google-maps-react'
 
 const mapStyles = {
   width:'100%',
@@ -7,6 +7,27 @@ const mapStyles = {
 }
 
 export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+  }
+
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    })
+  }
+
+  onClose = props => {
+    this.setState({
+      showingInfoWindow: false,
+      activeMarker: null
+    })
+  }
+
   render(){
     if (!this.props.loaded){
       return (
@@ -16,14 +37,27 @@ export class MapContainer extends Component {
     return (
       <div>
         <Map
-          google={this.props.google}
-          zoom={14}
-          style={mapStyles}
+          google = {this.props.google}
+          style = {mapStyles}
+          zoom = {14}
           initialCenter={{
             lat: 32.8138852,
             lng: -96.7683192
           }}
         />
+        <Marker
+          onClick = {this.onMarkerClick}
+          name = {'Lakewood Shopping Center'}
+        />
+        <InfoWindow
+          marker = {this.state.activeMarker}
+          visible = {this.state.showingInfoWindow}
+          onClose = {this.onClose}
+        />
+
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
       </div>
     )
   }
